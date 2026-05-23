876203cf7da45be515d075dd57df51cb943920a0
Iresha2002 <eg255887@engug.ruh.ac.lk>
2026-05-24 03:16:29 +0530

    fetch data from Dummyjson API

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+defineProps<{
+  title: string
+  price: number
+  image: string
+}>()
+
+</script>
+
+<template>
+
+  <div class="border p-5 rounded">
+
+    <img
+      :src="image"
+      class="h-40 w-full object-cover"
+    />
+
+    <h2 class="text-2xl font-bold mt-3">
+      {{ title }}
+    </h2>
+
+    <p class="text-green-500">
+      $ {{ price }}
+    </p>
+
+  </div>
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,52 @@
+<script setup lang="ts">
+
+import { ref, onMounted } from 'vue'
+import ProductCard from '../components/ProductCard.vue'
+
+type Product = {
+  id: number
+  title: string
+  price: number
+  thumbnail: string
+}
+const products = ref<Product[]>([])
+
+const fetchProducts = async () => {
+
+  const response = await fetch('https://dummyjson.com/products')
+
+  const data = await response.json()
+
+  products.value = data.products
+
+}
+
+onMounted(() => {
+  fetchProducts()
+})
+
+</script>
+
 <template>
+
   <div class="p-10">
-    <h1 class="text-4xl font-bold text-blue-500">
-      Home Page
+
+    <h1 class="text-4xl font-bold mb-5">
+      Products
     </h1>
+
+    <div class="grid grid-cols-3 gap-5">
+
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :title="product.title"
+        :price="product.price"
+        :image="product.thumbnail"
+      />
+
+    </div>
+
   </div>
+
 </template>
