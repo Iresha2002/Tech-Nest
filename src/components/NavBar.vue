6210b209bbebc29b6e4969f1aade0a5c21f2194a
Iresha2002 <eg255887@engug.ruh.ac.lk>
2026-05-25 19:46:02 +0530

    Show cart count in navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,27 @@
+<script setup lang="ts">
+
+import { useCartStore } from '../stores/cartStore'
+
+const cartStore = useCartStore()
+
+</script>
+
 <template>
+
   <nav class="p-5 bg-gray-800 text-white flex gap-5">
 
-    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/">
+      Home
+    </RouterLink>
 
-    <RouterLink to="/cart">Cart</RouterLink>
+    <RouterLink to="/cart">
+      Cart ({{ cartStore.items.length }})
+    </RouterLink>
 
-    <RouterLink to="/login">Login</RouterLink>
+    <RouterLink to="/login">
+      Login
+    </RouterLink>
 
   </nav>
+
 </template>
